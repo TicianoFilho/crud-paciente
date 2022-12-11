86b6be5b847e0773cdabc6ff9705e84f8a483f08
TicianoFilho <ticianofilho@gmail.com>
2022-12-11 19:39:36 -0300

    create: add RoleRepository

---
+++ src/main/java/br/com/mv/estagio/cubo/crudpaciente/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package br.com.mv.estagio.cubo.crudpaciente.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.mv.estagio.cubo.crudpaciente.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+
+	Role findByDescricao(String descricao);
+}
