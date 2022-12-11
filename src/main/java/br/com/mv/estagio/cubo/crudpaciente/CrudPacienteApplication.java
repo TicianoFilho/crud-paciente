8e37ac2a788172ee81bb3f2b643cfb54d5f1d27d
TicianoFilho <ticianofilho@gmail.com>
2022-12-11 15:56:03 -0300

    first commit

---
+++ src/main/java/br/com/mv/estagio/cubo/crudpaciente/CrudPacienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.mv.estagio.cubo.crudpaciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPacienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPacienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mv/estagio/cubo/crudpaciente/CrudPacienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mv.estagio.cubo.crudpaciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPacienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
