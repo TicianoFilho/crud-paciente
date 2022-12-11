57cd78be6959c013a04529488bbaf562b15c1918
TicianoFilho <ticianofilho@gmail.com>
2022-12-11 16:40:30 -0300

    create: add UsuarioController

---
+++ src/main/java/br/com/mv/estagio/cubo/crudpaciente/controllers/UsuarioController.java
@@ -0,0 +1,12 @@
+package br.com.mv.estagio.cubo.crudpaciente.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+}
