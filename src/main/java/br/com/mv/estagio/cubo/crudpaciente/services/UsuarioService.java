b32c09e039985e3cc43bff6577ea27362fc3ff0b
TicianoFilho <ticianofilho@gmail.com>
2022-12-11 19:51:32 -0300

    feat: add UsuarioService methods

--- src/main/java/br/com/mv/estagio/cubo/crudpaciente/services/UsuarioService.java
+++ src/main/java/br/com/mv/estagio/cubo/crudpaciente/services/UsuarioService.java
@@ -1,5 +1,15 @@
 package br.com.mv.estagio.cubo.crudpaciente.services;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import br.com.mv.estagio.cubo.crudpaciente.entities.Usuario;
+
 public interface UsuarioService {
 
+	Usuario saveUsuario(Usuario usuario);
+	void saveRoleParaUsuario(String email, String roleDescricao);
+	Usuario getUsuario(String email);
+	List<Usuario> getUsuarios(Page<Usuario> page);
 }
--- src/main/java/br/com/mv/estagio/cubo/crudpaciente/services/impl/UsuarioServiceImpl.java
+++ src/main/java/br/com/mv/estagio/cubo/crudpaciente/services/impl/UsuarioServiceImpl.java
@@ -1,7 +1,36 @@
 package br.com.mv.estagio.cubo.crudpaciente.services.impl;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import br.com.mv.estagio.cubo.crudpaciente.entities.Usuario;
 import br.com.mv.estagio.cubo.crudpaciente.services.UsuarioService;
 
 public class UsuarioServiceImpl implements UsuarioService {
+
+	@Override
+	public Usuario saveUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveRoleParaUsuario(String email, String roleDescricao) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Usuario getUsuario(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Usuario> getUsuarios(Page<Usuario> page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
