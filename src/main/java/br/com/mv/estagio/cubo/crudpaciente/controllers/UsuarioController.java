1646bac20ca8fc8b13f18592d75a007e1e7c678b
TicianoFilho <ticianofilho@gmail.com>
2022-12-17 12:28:00 -0300

    feat: add saveUsuarios method in UsuarioController

--- src/main/java/br/com/mv/estagio/cubo/crudpaciente/controllers/UsuarioController.java
+++ src/main/java/br/com/mv/estagio/cubo/crudpaciente/controllers/UsuarioController.java
@@ -1,13 +1,18 @@
 package br.com.mv.estagio.cubo.crudpaciente.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.mv.estagio.cubo.crudpaciente.domain.Usuario;
+import br.com.mv.estagio.cubo.crudpaciente.dtos.RoleToUserDto;
 import br.com.mv.estagio.cubo.crudpaciente.services.UsuarioService;
 import lombok.RequiredArgsConstructor;
 
@@ -21,5 +26,11 @@ public class UsuarioController {
 	@GetMapping("/usuarios")
 	public ResponseEntity<List<Usuario>> getUsuarios() {
 		return ResponseEntity.ok().body(usuarioService.getUsuarios());
+	}
+	
+	@PostMapping("/usuarios")
+	public ResponseEntity<Usuario> saveUsuario(@RequestBody Usuario usuario) {
+		URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/usuarios").toUriString());
+		return ResponseEntity.created(uri).body(usuarioService.saveUsuario(usuario));
 	}
 }
---
+++ src/main/java/br/com/mv/estagio/cubo/crudpaciente/dtos/RoleToUserDto.java
@@ -0,0 +1,10 @@
+package br.com.mv.estagio.cubo.crudpaciente.dtos;
+
+import lombok.Data;
+
+@Data
+public class RoleToUserDto {
+
+	private String emailUsuario;
+	private String descricaoRole;
+}
